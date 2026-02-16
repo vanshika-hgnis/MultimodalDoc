f0f13e8a66228f61a6af9db88a166f39e2815388
Vanshika Singh <vanshikas2018@gmail.com>
2026-02-16 23:48:57 +0530

    Phase 3 trying to implement celery , redis via docker cli.

--- api/celery_app.py
+++ api/celery_app.py
@@ -1,7 +1,9 @@
 from celery import Celery
 
+# broker="redis://<WSL_IP>:6379/0"
+# 172.19.108.1
 celery = Celery(
-    "worker", broker="redis://localhost:6379/0", backend="redis://localhost:6379/0"
+    "worker", broker="redis://172.19.108.1:6379/0", backend="redis://localhost:6379/0"
 )
 
 celery.conf.task_routes = {"tasks.ingest_document_task": {"queue": "ocr_queue"}}
---
+++ api/download_models.py
@@ -0,0 +1,5 @@
+# download_models.py
+from ocr_service import get_ocr_reader
+
+get_ocr_reader()
+print("Models initialized.")

0ceab2707a950bf538937fb70893ef5df598aba9
Vanshika Singh <vanshikas2018@gmail.com>
2026-02-26 22:59:27 +0530

    docker setup worker

--- api/celery_app.py
+++ api/celery_app.py
@@ -1,12 +1,15 @@
 # celery_app.py
 
+import os
 from celery import Celery
+
+broker_url = os.getenv("CELERY_BROKER_URL", "redis://redis:6379/0")
+result_backend = os.getenv("CELERY_RESULT_BACKEND", "redis://redis:6379/0")
 
 celery = Celery(
     "worker",
-    broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/0"
+    broker=broker_url,
+    backend=result_backend
 )
 
-# Tell celery where tasks are
 celery.conf.imports = ("tasks",)
